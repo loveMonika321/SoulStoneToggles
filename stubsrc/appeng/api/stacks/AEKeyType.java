a04ea4fd380d83aa986b30383c6ee7a60478bbce
loveMonika321 <loveMonika321@users.noreply.github.com>
2026-08-01 22:29:21 +0000

    移除 AE2 NPE 修复相关代码（作为独立 mod 发布）
    - 删除 mixin/ae2/AE2AbstractTableRendererMixin.java
    - 删除 stubsrc/appeng/ 编译桩
    - mixins.json 移除 ae2.AE2AbstractTableRendererMixin
    - build.gradle 移除 ae2-stub 依赖和编译任务

--- stubsrc/appeng/api/stacks/AEKeyType.java
+++
@@ -1,7 +0,0 @@
-package appeng.api.stacks;
-
-public interface AEKeyType {
-    java.util.Collection<? extends AEKeyType> VALUES = java.util.Collections.emptyList();
-    String getName();
-    AEKey loadKeyFromTag(net.minecraft.nbt.CompoundTag tag);
-}
